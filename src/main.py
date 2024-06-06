9e10431b88af9c7c27011542602fa8fae204ace2
Adefolaju <adefolajuariyo@gmail.com>
2024-06-06 09:51:52 +0100

    Static file copy feature

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
-from textnode import TextNode
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-  bold_textnode = TextNode("This is a text node", "bold", "https://www.adefolajuariyo.com")
+  print("Deleting public directory")
+  if os.path.exists(dir_path_public):
+    shutil.rmtree(dir_path_public)
 
-  print(bold_textnode)
-
+  print("Copying static files to public directory...")
+  copy_files_recursive(dir_path_static, dir_path_public)
 
 main()
